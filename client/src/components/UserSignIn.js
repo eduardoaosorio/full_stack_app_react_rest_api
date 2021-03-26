06f9c2ed58b1905f269924d75487c68da24c0825
Eduardo Osorio <eduardoaosorio95@gmail.com>
2021-03-26 18:10:05 -0500

    Enable sign in and form functionality

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,15 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useContext } from "react";
+import { Context } from "../Context";
+import { Link } from "react-router-dom";
 
-function UserSignIn() {
+function UserSignIn({ history, location }) {
+  const { actions } = useContext(Context);
+
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const { from } = location.state || { from: { pathname: "/" } };
+    actions
+      .signIn(emailAddress, password)
+      .then((user) => {
+        if (user === null) {
+          setErrors(["Sign-in was unsuccessful"]);
+        } else {
+          history.push(from);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        history.push("/error"); // ------------------------------------------------- DEFINIR A DONDE REDIRIJO
+      });
+  }
 
   return (
     <div className="bounds">
       <div className="grid-33 centered signin">
         <h1>Sign In</h1>
         <div>
-          <form>
+          {errors.length > 0 ? (
+            <div className="validation-errors">
+              <h2 className="validation--errors--label">Validation errors</h2>
+              <ul>
+                {errors.map((errorMsg) => (
+                  <li key={btoa(errorMsg)}>{errorMsg}</li>
+                ))}
+              </ul>
+            </div>
+          ) : null}
+          <form onSubmit={handleSubmit}>
             <div>
               <input
                 id="emailAddress"
@@ -18,6 +51,7 @@ function UserSignIn() {
                 className=""
                 placeholder="Email Address"
                 value={emailAddress}
+                onChange={(e) => setEmailAddress(e.target.value)}
               />
             </div>
             <div>
@@ -28,6 +62,7 @@ function UserSignIn() {
                 className=""
                 placeholder="Password"
                 value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <div className="grid-100 pad-bottom">
@@ -36,7 +71,10 @@ function UserSignIn() {
               </button>
               <button
                 className="button button-secondary"
-                onclick="event.preventDefault(); location.href='index.html';"
+                onClick={(e) => {
+                  e.preventDefault();
+                  history.push("/courses");
+                }}
               >
                 Cancel
               </button>
@@ -45,7 +83,7 @@ function UserSignIn() {
         </div>
         <p>&nbsp;</p>
         <p>
-          Don't have a user account? <a href="sign-up.html">Click here</a> to
+          Don't have a user account? <Link to="/signup">Click here</Link> to
           sign up!
         </p>
       </div>
